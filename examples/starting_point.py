a3a962bd43c8d0b33808ee7a77e989980996c2e4
David Nugent <david.nugent2425@gmail.com>
2021-04-06 16:08:09 +0100

    cleaned out unneeded stuff

---
+++ examples/starting_point.py
@@ -0,0 +1,10 @@
+
+class SampleDriver:    
+
+    def __init__(self):
+        self.sample_data = 0
+
+    def process_lidar(self, ranges):
+        speed = 5.0
+        steering_angle = 0.0
+        return speed, steering_angle
